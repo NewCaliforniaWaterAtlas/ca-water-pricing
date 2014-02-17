7e62dc03af77732b0bdd2812b85a97d1e8bb5780
videmsky <stuffaccounts@gmail.com>
2014-02-17 00:23:17 -0800

    marker scaling to rates & tooltips

--- public/js/directives/map.js
+++ public/js/directives/map.js
@@ -5,13 +5,13 @@
 app.directive('map', [ '$window','mapService', function ($window, mapService) {
 	return {
 		restrict: 'A',
-		replace: true,
+		// replace: true,
 		scope:{
       points:"="
     },
 		link: function(scope, element, attrs) {
 
-			mapService.map().then(function(map) {
+			mapService.map().then(function(map) {	
 
 				// watch for points changes and re-render
         scope.$watch('points', function (newData) {
@@ -19,7 +19,8 @@ app.directive('map', [ '$window','mapService', function ($window, mapService) {
         }, true);
 
         scope.render = function(points) {
-			    
+
+			    // check to see if points exist
         	if (!points) return; 	
 
 			    var popup = L.popup();
@@ -28,6 +29,8 @@ app.directive('map', [ '$window','mapService', function ($window, mapService) {
 			    // var bounds = new L.LatLngBounds(southWest, northEast);
 			    // L.Icon.Default.imagePath = '../img/leaflet';
 
+			    // setup map
+			    // todo: set bounds
 			    var map = L.map('map', {
 		        center: new L.LatLng(37.166111,-119.449444),
 		        zoom: 6,
@@ -49,17 +52,42 @@ app.directive('map', [ '$window','mapService', function ($window, mapService) {
 			    });
 			    tiles.addTo(map);
 
-			    var group = L.featureGroup();
-						
-	        angular.forEach(points, function(p, key){
-	          // var marker = L.marker([d.lat, d.lng]).addTo(group);
-	          var circle = L.circle([p.lat, p.lng], 500, {
-					    color: 'red',
-					    fillColor: '#f03',
-					    fillOpacity: 0.5
-	          }).addTo(group);
+			    // create feature group
+			    var pointGroup = L.featureGroup();
+
+			    // loop through points
+	        angular.forEach(points, function(point, key){
+	          //extend marker properties
+						var customCircleMarker = L.CircleMarker.extend({
+							options: { 
+								bill: point.bill,
+								rate: point.rate
+							}
+						});
+
+						var myMarker = new customCircleMarker([point.lat, point.lng], { 
+						  radius: point.rate * 3,
+						  color: "#fff",
+						  fillColor: "#1C75BC",
+						  fillOpacity: 0.9,
+						  opacity: 0.35
+						});
+
+	          myMarker.addTo(pointGroup);	
+
 	        });
-	        group.addTo(map);
+	     		// add circle markers to map
+	        pointGroup.addTo(map);
+	        //setup popups to trigger on mouseovers
+					pointGroup.on('mouseover', function(e) {
+					  var popup = L.popup()
+							.setLatLng(e.latlng) 
+							.setContent("Rate: " + "$ " + e.layer.options.rate + "/gal.");	
+						map.openPopup(popup);
+					})
+					.on('mouseout', function(e){
+						map.closePopup();
+					});
 
 				}// scope.render
 			
