a153fdc76eb813faaf723228a675db01a88398f9
videmsky <stuffaccounts@gmail.com>
2014-02-19 08:46:25 -0800

    added household size to schema

--- app/models/bill.js
+++ app/models/bill.js
@@ -5,13 +5,13 @@ module.exports = mongoose.model ('Bill', {
   bill: {type: Number, required: false },
   used: {type: Number, required: false },  
   bdate: {type: Number, required: false},
+  hsize: {type: Number, required: false },
   rate: {type: Number, required: false },
   zip: {type: Number, required: false },
-  // hsize: {type: Number, required: false },
   // zip: {type: Number, required: false },
   // coords: {lat: Number, lng: Number },
-  lat: {type: Number, required: false },
   lng: {type: Number, required: false },
+  lat: {type: Number, required: false },
   tstamp: { type : Date, default: Date.now },
   done : Boolean
 });
