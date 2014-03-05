cb4e9c5665f731fec7549bb212a8f234298f3d59
videmsky <stuffaccounts@gmail.com>
2014-03-05 11:43:21 -0800

    commented out local db

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 var credentials = require('../credentials');
 
 // the database url to connect
-module.exports = {
-
-dbPath : 'mongodb://localhost:27017/pricing'
-
-}
-
-
 // module.exports = {
 
-// dbPath : "mongodb://" + credentials.user + ":" + credentials.passwd + "@troup.mongohq.com:10031/db-ca-water-pricing"
+// dbPath : 'mongodb://localhost:27017/pricing'
 
 // }
+
+
+module.exports = {
+
+dbPath : "mongodb://" + credentials.user + ":" + credentials.passwd + "@troup.mongohq.com:10031/db-ca-water-pricing"
+
+}
 
