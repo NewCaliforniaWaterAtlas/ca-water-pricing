1d40bc2bd393e532974ee2adeee4e42fea75696b
videmsky <stuffaccounts@gmail.com>
2014-03-06 10:11:57 -0800

    path to production database

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 var credentials = require('../credentials');
 
-// the database url to connect
+// // local
 // module.exports = {
-
-// dbPath : 'mongodb://localhost:27017/pricing'
-
+// 	dbPath : 'mongodb://localhost:27017/pricing'
 // }
 
+// // sandbox
+// module.exports = {
+// 	dbPath : "mongodb://" + credentials.user + ":" + credentials.passwd + "@troup.mongohq.com:10031/db-ca-water-pricing"
+// }
 
+// production
 module.exports = {
-
-dbPath : "mongodb://" + credentials.user + ":" + credentials.passwd + "@troup.mongohq.com:10031/db-ca-water-pricing"
-
+	dbPath : "mongodb://" + credentials.user + ":" + credentials.passwd + "@emma.mongohq.com:10009/db-ca-water-pricing_production"
 }
-
