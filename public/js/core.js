40ce5d090144797875121343f7c398b55837fea2
videmsky <stuffaccounts@gmail.com>
2014-04-14 08:11:33 -0700

    reverted base url

--- public/js/core.js
+++ public/js/core.js
@@ -9,7 +9,8 @@ app.config( function ($stateProvider, $urlRouterProvider, $locationProvider) {
 	
 	$stateProvider
 		.state('index', {
-			url: '/',
+			// url: '/',
+			url: '',
 			templateUrl: 'views/templates/home.html', 
 			controller: 'myModal'
 		})
@@ -32,8 +33,8 @@ app.config( function ($stateProvider, $urlRouterProvider, $locationProvider) {
 		// 	controller: 'mapController'
 		// });
 
-	if (window.history && window.history.pushState) {
-  	$locationProvider.html5Mode(true).hashPrefix('!');
-	}
+	// if (window.history && window.history.pushState) {
+ //  	$locationProvider.html5Mode(true).hashPrefix('!');
+	// }
 
 });
