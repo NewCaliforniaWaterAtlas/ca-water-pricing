0046065a00baa609f0d02adfb9427d1103dd61ad
videmsky <stuffaccounts@gmail.com>
2014-04-14 00:01:02 -0700

    forgot / in route

--- app/routes.js
+++ app/routes.js
@@ -149,13 +149,8 @@ module.exports = function(app) {
 	
 
 	// application -------------------------------------------------------------
-	// app.get('*', function(req, res) {
-	// 	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-	// });
-
-
-	app.get('*', function(req, res) {
-	     res.sendfile('index.html', { root: './public' });
+	app.get('/*', function(req, res) {
+		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 
 	// schedule request to NOAA REST API & return Palmer Drought Serverity Index
