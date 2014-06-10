227a5686814fa37421df599c7692746098e67a17
videmsky <stuffaccounts@gmail.com>
2014-06-09 23:07:49 -0700

    more route tweaks

--- public/js/core.js
+++ public/js/core.js
@@ -5,8 +5,13 @@ var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'ngAutocomplete', 
 
 app.config( ['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
 	
-	$urlRouterProvider.otherwise('/');
-	
+	// if (window.history && window.history.pushState) {
+ //  	$locationProvider.html5Mode(true);
+ //  	$locationProvider.hashPrefix('!');
+	// }
+
+	// $locationProvider.html5Mode(true).hashPrefix('!');
+
 	$stateProvider
 		.state('index', {
 			url: '/',
@@ -32,8 +37,6 @@ app.config( ['$stateProvider', '$urlRouterProvider', '$locationProvider', functi
 		// 	controller: 'mapController'
 		// });
 
-	if (window.history && window.history.pushState) {
-  	$locationProvider.html5Mode(true);
-	}
+		$urlRouterProvider.otherwise('/');
 
 }]);
