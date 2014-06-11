f61227aa0b0deef7366478ca700b2a43db73e933
videmsky <stuffaccounts@gmail.com>
2014-06-10 22:29:48 -0700

    removed middleware

--- app/routes.js
+++ app/routes.js
@@ -156,7 +156,8 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		// res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('index.html', { root: './public' });
 	});
 
 	// schedule request to NOAA REST API & return Palmer Drought Serverity Index
